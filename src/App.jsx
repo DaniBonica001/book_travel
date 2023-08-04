3d04ec4e8713d861cc4dbcd9e1f9d2a8da934648
DaniBonica001 <danielabc_0106@hotmail.com>
2023-08-04 11:03:27 -0500

    css, App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import NavBar from './Components/Navbar/NavBar'
+import Home from './Components/Home/Home' 
+import Main from './Components/Main/Main'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   
-  return <div>Hola</div>;
+  return <>
+  <NavBar/>
+  <Home/>
+  <Main/>
+  <Footer/>
+  </>
 }
 
 export default App;
