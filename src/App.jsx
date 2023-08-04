a73df79ad685984d4e3495c8ea580b90387d17e6
DaniBonica001 <danielabc_0106@hotmail.com>
2023-08-04 12:04:21 -0500

    navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   
   return <>
   <NavBar/>
-  <Home/>
+{/*   <Home/>
   <Main/>
-  <Footer/>
+  <Footer/> */}
   </>
 }
 
--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,9 +1,81 @@
-import './navbar.css';
+import "./navbar.css";
+import { useState } from "react";
+import { MdTravelExplore } from "react-icons/md";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TbGridDots } from "react-icons/tb";
 
 function NavBar() {
+  const [active, setActive] = useState("navBar");
+
+  //Function to toggle navBar
+  const toggleNavbar = () => {
+    if (active === "navBar") {
+      setActive("navBar activeNavbar");
+    } else {
+      setActive("navBar");
+    }
+  };
+
   return (
-    <div>NavBar</div>
-  )
+    <section className="navBarSection">
+      <header className="header flex">
+        <div className="logoDiv">
+          <a href="#" className="logo flex">
+            <h1>
+              <MdTravelExplore className="icon" /> Travel.
+            </h1>
+          </a>
+        </div>
+
+        <div className={active}>
+          <ul className="navLists flex">
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Home
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Packages
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Shop
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                About
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Pages
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Contact
+              </a>
+            </li>
+
+            <button className="btn">
+              <a href="">BOOK NOW</a>
+            </button>
+          </ul>
+
+          <div onClick={toggleNavbar} className="closeNavbar">
+            <AiFillCloseCircle className="icon" />
+          </div>
+        </div>
+
+        <div onClick={toggleNavbar} className="toggleNavbar">
+          <TbGridDots className="icon" />
+        </div>
+      </header>
+    </section>
+  );
 }
 
-export default NavBar
+export default NavBar;
