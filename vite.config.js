c14baf6e729d4e16b4aaedec3881670baeabf2d4
DaniBonica001 <danielabc_0106@hotmail.com>
2023-08-25 21:07:11 -0500

    base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://danibonica001.github.io/book_travel/'
 })
